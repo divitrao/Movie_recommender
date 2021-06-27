55a0acc419426cc1ddf6946f5a72d5c64adfb04e
divitrao <divitrao97d@gmail.com>
2021-06-27 18:49:08 +0530

    started to create a home page

---
+++ movies/recommend/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+# Create your views here.
+#https://yts.mx/api/v2/list_movies.json?page=600&limit=50
+#https://www.imdb.com/feature/genre/
+
+from django.views.generic import TemplateView
+
+class HomePage(TemplateView):
+    template_name = 'home.html'
