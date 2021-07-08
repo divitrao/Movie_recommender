b407af344c395f1518e9fc251ba1a2357c058c29
divitrao <divitrao97d@gmail.com>
2021-07-08 18:46:11 +0530

    added YTS api

--- movies/recommend/views.py
+++ movies/recommend/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+# import requests
 
 # Create your views here.
 #https://yts.mx/api/v2/list_movies.json?page=600&limit=50
@@ -7,4 +8,10 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HomePage(TemplateView):
+    # base_url = 'https://yts.mx/api/v2/list_movies.json?'
+    # r = requests.get(base_url , params={'page':500,'limit':50})
+    # print(r)
+    # info = r.json()
+    # # print(json.dumps(info,indent=4))
+    # print(info['data']['movies'][0])
     template_name = 'home.html'
